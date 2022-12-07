4dc7ff71c379f2f3785ae3b1d0e9e4e53b12b642
grbllcnls <gabriellecanlas6@gmail.com>
2022-12-07 17:23:43 +0800

    working login

--- dbConnection.php
+++ dbConnection.php
@@ -1,20 +1,15 @@
 <?php
 
-$sname= 'localhost';
+$host = "localhost";  
+$user = "root";  
+$password = '';  
+$db_name = "csids";  
+  
+$con = mysqli_connect($host, $user, $password, $db_name, 3307);  
+if(mysqli_connect_errno()) {  
+  die("Failed to connect with MySQL: ". mysqli_connect_error());  
+}  
 
-$uname= 'root';
-
-$password = '';
-
-$db_name = 'csids';
 
 
-
-$conn = mysqli_connect($sname, $uname, $password, $db_name, 3307);
-
-if ($conn) {
-    echo "Connected!";
-  } else {
-    echo "Connection Failed";
-  }
   ?>
