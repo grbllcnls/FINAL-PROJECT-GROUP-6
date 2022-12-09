acef2158d28c4a34c9115074bcf1fbe4755163c6
vince <vincepena1829@gmail.com>
2022-12-09 13:40:49 +0800

    bestdeals

---
+++ navbar2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+      <li ><a href="bestsellers.php">BEST SELLERS</a></li>
+      <li><a href="buildyourownpc.php">BUILD YOUR OWN PC</a></li>
+      <li class="active"><a href="bestdeals.php">BEST DEALS</a></li>
+      <li><a href="budgetfriendlybuilds.php">BUDGET FRIENDLY BUILDS</a></li>
+      <li><a href="rtx.php">RTX 4090 GAMING PCS</a></li>
+      <li><a href="latestbuilds.php">LATEST BUILDS</a></li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
